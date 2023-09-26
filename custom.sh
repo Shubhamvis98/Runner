0546e179772c037da5a86ba52da4c6f4bb5cc5e6
ShubhamVis98 <43336210+Shubhamvis98@users.noreply.github.com>
2023-09-26 20:32:27 +0530

    init

---
+++ custom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+mytmp=/ztmpz
+cd $mytmp
+apt install -y python-is-python3 python3-psutil macchanger mdk4 aircrack-ng gir1.2-notify-0.7 git
+git clone https://github.com/shubhamvis98/nethunter-pinephone nhpp
+cd nhpp; ./install.sh
+
+wget https://github.com/Shubhamvis98/Only4Me/raw/master/ttyescape.tar.gz -O ttyesc.tgz && \
+tar -C / -xpf ttyesc.tgz && \
+systemctl enable hkdm
