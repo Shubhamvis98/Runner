3f60f1b32a8daae10fb54bad9a63813f565df28c
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2023-09-27 19:08:14 +0530

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,13 +3,13 @@
 export DEBIAN_FRONTEND=noninteractive
 mytmp=/ztmpz
 cd $mytmp
-apt install -y python-is-python3 python3-psutil macchanger mdk4 aircrack-ng gir1.2-notify-0.7 git
+apt install -y python-is-python3 python3-psutil macchanger mdk4 aircrack-ng gir1.2-notify-0.7 git kbd
 git clone https://github.com/shubhamvis98/nethunter-pinephone nhpp
 cd nhpp; ./install.sh
 
-wget https://github.com/Shubhamvis98/Only4Me/raw/master/ttyescape.tar.gz -O ttyesc.tgz && \
-tar -C / -xpf ttyesc.tgz && \
-systemctl enable hkdm
+wget https://github.com/Shubhamvis98/PinePhone_Tweaks/raw/main/ttyescape.tar.gz -O ttyesc.tgz && \
+  tar -C / -xpf ttyesc.tgz && \
+  systemctl enable hkdm
 
 sed -i 's/-0.07/0/;s/-0.13/0/' /usr/share/plymouth/themes/kali/kali.script || true
 
